71869a3fad45cfdb77996e92ed4b2246f149ec80
LW <iwhere@IwheredeiMac-2.local>
2018-08-13 09:41:29 +0800

    no message

---
+++ SD_webImageviewTest/SD_webImageviewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SD_webImageviewTest
+//
+//  Created by LW on 2018/8/13.
+//  Copyright © 2018年 iWhere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SD_webImageviewTest/SD_webImageviewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SD_webImageviewTest
+//
+//  Created by LW on 2018/8/13.
+//  Copyright © 2018年 iWhere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
